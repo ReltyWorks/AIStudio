189b1961d83f3e6e9a85253487744a5af3aa692e
Relty11812 <relty.works@gmail.com>
2025-12-11 13:44:40 +0900

    TalkToCatAI

---
+++ TalkToCatAI/Program.cs
@@ -0,0 +1,30 @@
+﻿using GenerativeAI;
+using GenerativeAI.Types;
+
+namespace TalkToCatAI
+{
+    class Program
+    {
+        private static string _apiKey = "api";
+        private static string _persona = "너는 아주 똑똑한 말하는 고양이이야. 문장 끝마다 '냥'을 붙여서 대답해.";
+        private static GoogleAi _googleAi = new GoogleAi(_apiKey);
+
+        private static async Task Main(string[] args)
+        {
+            string message;
+            GenerativeModel gemini = _googleAi.CreateGenerativeModel("models/gemini-2.5-flash-lite");
+            gemini.SystemInstruction = _persona;
+
+            ChatSession catAI = gemini.StartChat();
+
+            while (true)
+            {
+                Console.Write("사용자 : ");
+                message = Console.ReadLine();
+
+                GenerateContentResponse response = await catAI.GenerateContentAsync(message);
+                Console.WriteLine($"고양이 : {response.Text}");
+            }
+        }
+    }
+}
